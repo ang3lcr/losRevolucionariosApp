5aec6b6fb054fa998c6b863ceeec6e291639e4e3
ang3lcr <ang3lcortes@outlook.com>
2024-05-06 07:15:46 -0700

    footer bg fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import Logo from "/logoLetters.png"
+import Logo from "/logoHero.png"
 const Footer = () => {
   return (
-    <div className='flex justify-between items-center absolute bottom-100 w-full border-solid bg-black mt-10'>
-      <img src={Logo} alt="" className='w-1/3 ml-20'/>
+    <div className='flex justify-between items-center absolute bottom-100 w-full border-solid bg-neutral-900 mt-10'>
+      <img src={Logo} alt="" className='size-40 ml-20 p-2'/>
       <div className='flex justify-center items-center mr-20 flex-col'>
           <p className='font-sancreek text-white text-center mt-5 text-2xl'>Contacto: 4432324326</p>
           <ul className='flex justify-between gap-5 mt-5 mb-5'>
-            <li className='bg-red-800 w-16 h-16 rounded-full'>1</li>
-            <li className='bg-red-800 w-16 h-16 rounded-full'>2</li>
-            <li className='bg-red-800 w-16 h-16 rounded-full'>3</li>
-            <li className='bg-red-800 w-16 h-16 rounded-full'>4</li>
-            <li className='bg-red-800 w-16 h-16 rounded-full'>5</li>
+            <li className='bg-orange-400 w-16 h-16 rounded-full'></li>
+            <li className='bg-orange-400 w-16 h-16 rounded-full'></li>
+            <li className='bg-orange-400 w-16 h-16 rounded-full'></li>
+            <li className='bg-orange-400 w-16 h-16 rounded-full'></li>
+            <li className='bg-orange-400 w-16 h-16 rounded-full'></li>
           </ul>
       </div>
     </div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Logo from "/logoLetters.png"
+import Logo from "/logoHero.png"
 
 
 const NavBar = () => {
   return (
-    <section className='flex justify-between items-center h-15 bg-neutral-950 bg-opacity-50 absolute'>
-      <img src={Logo} alt="" className='pl-10 w-3/12'/>
+    <section className='flex justify-between items-center bg-neutral-950 bg-opacity-50 absolute w-full'>
+      <img src={Logo} alt="" className='pl-10 w-40'/>
         <ul className='flex justify-between w-4/12 pr-10'>
           <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Quienes Somos</li>
           <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Menu</li>
