1f5e20b072fdf66ef43a4cbeed543d3a69424475
ang3lcr <ang3lcortes@outlook.com>
2024-05-06 10:04:50 -0600

    fixed color

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Logo from "/logoHero.png"
+import Logo from "/logoLetters.png"
 const Footer = () => {
   return (
-    <div className='flex justify-between items-center absolute bottom-100 w-full border-solid bg-zinc-300 mt-10'>
-      <img src={Logo} alt="" className='size-40 ml-20 p-2'/>
+    <div className='flex justify-between items-center absolute bottom-100 w-full border-solid bg-zinc-900 mt-10'>
+      <img src={Logo} alt="" className='size-1/4 ml-20 p-2'/>
       <div className='flex justify-center items-center mr-20 flex-col'>
           <p className='font-sancreek text-white text-center mt-5 text-2xl'>Contacto: 4432324326</p>
           <ul className='flex justify-between gap-5 mt-5 mb-5'>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Logo from "/logoHero.png"
+import Logo from "/logoLetters.png"
 
 
 const NavBar = () => {
   return (
-    <section className='flex justify-between items-center bg-zinc-300 bg-opacity-50 absolute w-full'>
-      <img src={Logo} alt="" className='pl-10 w-40'/>
+    <section className='flex justify-between items-center bg-zinc-300 bg-opacity-10 absolute w-full'>
+      <img src={Logo} alt="" className='pl-10 w-1/4'/>
         <ul className='flex justify-between w-4/12 pr-10'>
           <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Quienes Somos</li>
           <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Menu</li>
