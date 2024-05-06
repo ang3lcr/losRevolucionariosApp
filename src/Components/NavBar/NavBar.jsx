ec08b4ffe67ce6f2ea88f4f0d708d584068d3102
ang3lcr <ang3lcortes@outlook.com>
2024-05-06 09:56:37 -0600

    fixed color navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import Logo from "/logoHero.png"
 
 const NavBar = () => {
   return (
-    <section className='flex justify-between items-center bg-zinc-900 bg-opacity-50 absolute w-full'>
+    <section className='flex justify-between items-center bg-zinc-300 bg-opacity-50 absolute w-full'>
       <img src={Logo} alt="" className='pl-10 w-40'/>
         <ul className='flex justify-between w-4/12 pr-10'>
           <li className='bg-red-900 p-3 rounded-full text-white w-3/12 text-center items-center uppercase'>Quienes Somos</li>
