ee518eec519b1459dd9c9753dcc72a6cd1c85fcd
ang3lcr <ang3lcortes@outlook.com>
2024-05-16 01:57:48 -0700

    menu responsive

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -12,57 +12,39 @@ const Menu = () => {
     axios.get("https://losrevolucionariosapi.onrender.com/api/v1/food")
       .then(res => setFood(res.data))
   },[])
-
-  console.log(food)
-
-
-
-
   return (
-      // <div className='flex flex-col justify-center items-center h-full w-screen'>
-      // {categories.map(category => {
-      //   if(category.category == "especialidad" || category.category == "parrilla" || category.category == "cocteles"){
-      //     return (
-      //       <section className='bg-gray-400 w-full flex flex-col items-center mt-16'>
-      //         <SpecialText text={category.category}/>
-      //         <div className='w-4/6 h-96 bg-green-300 mb-20'>
-      //           <img src={category.imageUrl} alt="" className='w-full h-full object-cover object-center'/>
-      //         </div>
-      //         <ul className='mb-20'>
-      //           <li>
-      //             <p>öla</p>
-      //           </li>
-      //         </ul>
-      //   </section>
-      //     )
-      //   }
-      // })}
-      // </div>
-      <div className='bg-slate-100 flex flex-col justify-center items-center w-full'>
-        {categories.map(category => {
-          if(category.category == "especialidad" || category.category == "parrilla" || category.category == "cocteles")
-            return (
-              <div className='flex flex-col justify-center items-center w-2/3 bg-slate-50 text-xl'> 
-              <SpecialText text={category.category.toUpperCase()} />
-              <div className='w-2/3 h-96 bg-black'>
-                <img src={category.imageUrl} alt="" className='w-full h-full object-cover'/>
-              </div>
-              <ul className='w-2/3 m-5 pl-10'>
-                {food.map(e => {
-                  if(e.foodCategory == category.id){
-                    return(
-                      <li className='mt-1'>
-                        <p className='font-akshar w-fit h-full text-3xl'>{e.name.toUpperCase()}</p>
-                        <p className='mt-0 h-fit'>({e.ingredients.toUpperCase()})</p>
-                      </li>
-                    )
-                  }
-                })}
-              </ul>
+
+    <div className='bg-slate-100 flex flex-col justify-center items-center w-full'>
+    {categories.map(category => {
+      if(category.category === "especialidad" || category.category === "parrilla" || category.category === "cocteles") {
+        return (
+          <div key={category.id} className='w-full md:w-2/3 lg:w-1/2 xl:w-2/3 flex flex-col justify-center items-center bg-slate-50 text-xl p-4'>
+            <div className='w-full flex justify-center'>
+              <SpecialText text={category.category.toUpperCase()}/>
             </div>
-            )
-        })}
-      </div>     
+            <div className='w-full h-60 md:h-80 lg:h-96 xl:w-2/3 bg-black'>
+              <img src={category.imageUrl} alt="" className='w-full h-full object-cover'/>
+            </div>
+            <ul className='w-full m-3 md:m-5 pl-5 md:pl-10 xl:w-2/3 flex flex-col justify-center'>
+              {food.map(e => {
+                if(e.foodCategory === category.id){
+                  return (
+                    <li key={e.id} className='mt-1 xl:w-2/3'>
+                      <p className='font-akshar text-2xl md:text-3xl lg:text-4xl'>{e.name.toUpperCase()}</p>
+                      <p className='mt-0 text-xs md:text-sm'>({e.ingredients.toUpperCase()})</p>
+                    </li>
+                  )
+                }
+              })}
+            </ul>
+          </div>
+        )
+      }
+      return null;
+    })}
+    
+  </div>
+      
   )
 }
 
