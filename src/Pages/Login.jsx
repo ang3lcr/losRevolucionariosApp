cc8fa1caca2932926b074faca224f66c9bdf8af3
ang3lcr <ang3lcortes@outlook.com>
2024-05-28 08:58:28 -0700

    captcha

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,33 +2,53 @@ import React, { useState } from 'react';
 import LogoPrincipal from "../../public/logo.jpeg";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { useRef } from "react";
 
 
 const Login = () => {
   const [username, setUserName] = useState('')
   const [password, setPassword] = useState('')
+  const [validCaptcha, setValidCaptcha] = useState(null)
+  const [validUser, setValidUser] = useState(false)
+
+  const captcha = useRef(null)
 
   const navigate = useNavigate();
 
 
   const submit = () => {
-    let data = {
-      "username": username,
-      "password": password
+    if(captcha.current.getValue()){
+      let data = {
+        "username": username,
+        "password": password
+      }
+      axios.post("https://losrevolucionariosapi.onrender.com/api/v1/auth/login",data).then(res => {
+        localStorage.setItem("token", res.data.token);
+        navigate("/admin");
+      })
+    } else {
+      console.log("acepta el captcha")
     }
-    axios.post("https://losrevolucionariosapi.onrender.com/api/v1/auth/login",data).then(res => {
-      localStorage.setItem("token", res.data.token);
-      navigate("/admin");
-    })
   }
+
+  const test = () => {
+    if(captcha.current.getValue()){
+
+    }
+  }
+
+
+
 
   return (
     <div className="bg-red-700 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md w-80 flex flex-col">
+      <div className="bg-white p-8 rounded-lg shadow-md w-90 flex flex-col">
         <div className="flex justify-center mb-8 items-center">
           <img src={LogoPrincipal} alt="Logo" className="h-20" />
         </div>
         <h1 className="text-xl font-semibold mb-6 text-center">Iniciar Sesión</h1>
+        {!validUser &&
         <div className="text-center">
           <div className="mb-4">
             <input
@@ -48,10 +68,18 @@ const Login = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
+          <div id='REcaptcha'>
+            <ReCAPTCHA
+              ref={captcha} 
+              sitekey='6Ld_zeopAAAAADVx5hQmmvgLBVZigS0RXEcNOSp4'
+              onChange={() => test()}
+            />
+          </div>
           <button className="btn btn-primary btn-large btn-block mb-4" onClick={() => submit()}>
             Iniciar Sesión
           </button>
         </div>
+        }
       </div>
     </div>
   );
